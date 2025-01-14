02610f107daf6ada2f6d5fca1412ec66cde91080
User <User@ASUS>
2025-01-14 14:14:56 +0200

    Add project files.

---
+++ Models/Producator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proiect.Models
+{
+    public class Producator
+    {
+        public int ID { get; set; }
+        [Display(Name = "Producer Name")]
+        public string NumeProducator { get; set; }
+        
+        public ICollection<Produs> Produse { get; set; }
+    }
+}
---
+++ Models/Produs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proiect.Models
+{
+    public class Produs
+    {
+        public int ID { get; set; }
+        [Required, StringLength(150, MinimumLength = 3)]
+        [Display(Name = "Product Name")]
+        public string DenumireProdus { get; set; }
+        public string Descriere { get; set; }
+        [Range(1, 300)]
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Pret { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Expiration Date")]
+        public DateTime PublishingDate { get; set; }
+        public int ProducatorID { get; set; }
+        public Producator Producator { get; set; }
+        [Display(Name = "Product Name")]
+        public ICollection<CategorieProdus> CategoriiProdus { get; set; }
+    }
+}
